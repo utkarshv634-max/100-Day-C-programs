2dbeeab19ab5fe1ea490c28de8fe3a3f83be4647
Utkarsh Verma <utkarshv634@gmail.com>
2026-08-18 21:59:13 +0530

    Add Q15 and Q16 and Q18

---
+++ Q18_Grade_Calculator.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    float percentage;
+    printf("Enter percentage: ");
+    scanf("%f", &percentage);
+    if (percentage >= 90 && percentage <= 100)
+    {
+        printf("Grade A\n");
+    }
+    else if (percentage >= 80 && percentage < 90)
+    {
+        printf("Grade B/n");
+    }
+    else if (percentage >= 70 && percentage < 80)
+    {
+        printf("Grade C\n");
+    }
+    else if (percentage >= 60 && percentage < 70)
+    {
+        printf("Grade D/n");
+    }
+    else if (percentage >= 0)
+    {
+        printf("Grade F/n");
+    }
+    else
+    {
+        printf("Invalid percentage\n");
+    }
+    return 0;
+}
